2653f3e6ababc1dfba3dcaca767cb8555f7d3d34
Sai Nareen Purimetla <sainareen06@gmail.com>
2025-04-01 17:35:06 -0400

    second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavbarComponent from './components/NavbarComponent';
+import HomePage from './components/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+     <NavbarComponent />
+     <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+
+export default function HomePage() {
+    return (
+        <>
+        <div>
+        <h1>Welcome to the Our Page</h1>
+        <p>This is the content of the home page.</p>
+        </div>
+
+        <div className="about-section">
+        <h2>About Us</h2>
+        <p>We are a company that specializes in providing high-quality products and services.</p>
+        </div>
+        </>
+    );
+}
